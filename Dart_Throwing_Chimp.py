2f2c6010d15bd7faaa8f1f43dfe8b9df2fb6176d
Zarak <zarak@hotmail.ca>
2020-01-11 21:09:44 +0500

    Add brier score calculation for 3 bins

--- Dart_Throwing_Chimp.py
+++ Dart_Throwing_Chimp.py
@@ -23,8 +23,8 @@ EPSILON = 0.05
 
 def FS(F, P, m):
     """    Calculates Fair Skill score with log base 2 score
-    f = frequency vector of actual outcomes
-    p = probability vector of mutually exclusive events
+    F = frequency vector of actual outcomes
+    P = probability vector of mutually exclusive events
     m = number of mutually exclusive events
     check if it works even when y has both 0 and 1 values?
     """
@@ -51,19 +51,37 @@ def FS2(f, x, y):
     return f1 * np.log2(x) + f2 * np.log2(y) + f3 * np.log2(1 - x - y) + np.log2(3)
 
 
-def BS(f, x, y):
+def BS(F, P, m):
     """
     Calculates Brier score
 
     Args:
-        f (ndarray) : frequency vector of actual outcomes
-        x, y (ndarray) : linspace (0.01, 0.99) with 99 points
+        F (ndarray) : frequency vector of actual outcomes
+        P : probability vector of mutually exclusive events
+        m : number of mutually exclusive events
 
     Returns:
         Array with Brier score calculation
     """
     pass
 
+
+def BS2(f, x, y):
+    """
+    Calculates Fair Skill score with log base 2 score
+
+    Args:
+        f (ndarray) : frequency vector of actual outcomes
+        x, y (ndarray) : linspace (0.01, 0.99) with 99 points
+
+    Returns:
+        Array with Fair score calculation
+    """
+    f1, f2, f3 = f
+    z = f1 * (1 - x)**2 + f2 * (1 - y)**2 + f3 * (x + y)**2
+    mask = (x + y) > 1
+    z[mask] = np.nan
+    return z
 
 #3-bin example
 m = 3 #mutually excluding events
@@ -116,16 +134,21 @@ def linear_space(m):
     return np.meshgrid(*P, sparse=True)
 
 
-def dart_probability(z):
+def dart_probability(z, method='fs'):
     n = 99
-    space = n * (n + 1) / 2
-    return np.sum(z > 0) / space
+    if method == 'fs':
+        space = n * (n + 1) / 2
+        return np.sum(z > 0) / space
+    elif method == 'bs':
+        space = n * (n + 1) / 2 + n * 2 + 3
+        return np.sum(z < (1 - 1/3)**2) / space
 
 
-def plot_2D(P, z):
+def plot_2D(P, z, title):
     p, _ = P
     p = p.squeeze()
     h = plt.contourf(p, p, z)
+    plt.title(title)
     plt.show()
 
 
@@ -134,11 +157,16 @@ if __name__ == "__main__":
     # F = [0.2, 0.2, 0.4, 0.1, 0.1]
     assert sum(F) == 1, "Frequency of each event, must sum to 1 = 100% prob."
     m = len(F)
-    # xx, yy = linear_space(m)
+    xx, yy = linear_space(m)
+
     P = linear_space(m)
-    z = FS(F, P, m)
-    prob = dart_probability(z)
-    print(f"Probability of having FS > 0 is {prob}")
+    fs = FS(F, P, m)
+    bs = BS2(F, xx, yy)
+    prob_fs = dart_probability(fs, method='fs')
+    prob_bs = dart_probability(bs, method='bs')
+    print(f"Probability of having FS > 0 is {prob_fs}")
+    print(f"Probability of having BS < 0.444 is {prob_bs}")
 
     if m == 3:
-        plot_2D(P, z)
+        plot_2D(P, fs, title='Fair Skill')
+        plot_2D(P, bs, title='Brier Score')
